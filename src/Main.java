49e00f3cd5e17bb50aaa76084818417ad94e87dd
haripriya-0712 <haripriyasudhakar0712>
2026-04-01 09:18:07 +0530

    TrainConistuc5

--- src/Main.java
+++ src/Main.java
@@ -1,79 +1,14 @@
-abstract class Room {
-    protected String roomType;
-    protected int beds;
-    protected double price;
+import java.util.LinkedHashSet;
 
-    public Room(String roomType, int beds, double price) {
-        this.roomType = roomType;
-        this.beds = beds;
-        this.price = price;
-    }
-
-    public abstract void displayRoomDetails();
-}
-
-class SingleRoom extends Room {
-
-    public SingleRoom() {
-        super("Single Room", 1, 1000);
-    }
-
-    public void displayRoomDetails() {
-        System.out.println("Room Type: " + roomType);
-        System.out.println("Beds: " + beds);
-        System.out.println("Price: ₹" + price);
-    }
-}
-
-class DoubleRoom extends Room {
-
-    public DoubleRoom() {
-        super("Double Room", 2, 2000);
-    }
-
-    public void displayRoomDetails() {
-        System.out.println("Room Type: " + roomType);
-        System.out.println("Beds: " + beds);
-        System.out.println("Price: ₹" + price);
-    }
-}
-
-class SuiteRoom extends Room {
-
-    public SuiteRoom() {
-        super("Suite Room", 3, 5000);
-    }
-
-    public void displayRoomDetails() {
-        System.out.println("Room Type: " + roomType);
-        System.out.println("Beds: " + beds);
-        System.out.println("Price: ₹" + price);
-    }
-}
-
-public class UseCase2RoomInitialization {
-
+public class TrainConsistUC5 {
     public static void main(String[] args) {
-
-        Room single = new SingleRoom();
-        Room doubleRoom = new DoubleRoom();
-        Room suite = new SuiteRoom();
-
-        int singleAvailable = 5;
-        int doubleAvailable = 3;
-        int suiteAvailable = 2;
-
-        System.out.println("=== Hotel Room Details ===\n");
-
-        single.displayRoomDetails();
-        System.out.println("Available: " + singleAvailable + "\n");
-
-        doubleRoom.displayRoomDetails();
-        System.out.println("Available: " + doubleAvailable + "\n");
-
-        suite.displayRoomDetails();
-        System.out.println("Available: " + suiteAvailable + "\n");
-
-        System.out.println("Application Finished.");
+        LinkedHashSet<String> train = new LinkedHashSet<>();
+        train.add("Engine");
+        train.add("Sleeper");
+        train.add("Cargo");
+        train.add("Guard");
+        train.add("Sleeper");
+        System.out.println("Final Train Formation:");
+        System.out.println(train);
     }
 }
