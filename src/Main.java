b9aa903a6ec09704e384e0811c41eafe509b6e04
haripriya-0712 <haripriyasudhakar0712>
2026-04-01 09:07:40 +0530

    TrainConsistUC4

--- src/Main.java
+++ src/Main.java
@@ -1,79 +1,24 @@
-abstract class Room {
-    protected String roomType;
-    protected int beds;
-    protected double price;
+import java.util.LinkedList;
 
-    public Room(String roomType, int beds, double price) {
-        this.roomType = roomType;
-        this.beds = beds;
-        this.price = price;
-    }
-
-    public abstract void displayRoomDetails();
-}
-
-class SingleRoom extends Room {
-
-    public SingleRoom() {
-        super("Single Room", 1, 1000);
-    }
-
-    public void displayRoomDetails() {
-        System.out.println("Room Type: " + roomType);
-        System.out.println("Beds: " + beds);
-        System.out.println("Price: ₹" + price);
-    }
-}
-
-class DoubleRoom extends Room {
-
-    public DoubleRoom() {
-        super("Double Room", 2, 2000);
-    }
-
-    public void displayRoomDetails() {
-        System.out.println("Room Type: " + roomType);
-        System.out.println("Beds: " + beds);
-        System.out.println("Price: ₹" + price);
-    }
-}
-
-class SuiteRoom extends Room {
-
-    public SuiteRoom() {
-        super("Suite Room", 3, 5000);
-    }
-
-    public void displayRoomDetails() {
-        System.out.println("Room Type: " + roomType);
-        System.out.println("Beds: " + beds);
-        System.out.println("Price: ₹" + price);
-    }
-}
-
-public class UseCase2RoomInitialization {
-
+public class TrainConsistUC4 {
     public static void main(String[] args) {
 
-        Room single = new SingleRoom();
-        Room doubleRoom = new DoubleRoom();
-        Room suite = new SuiteRoom();
+        LinkedList<String> train = new LinkedList<>();
+        train.add("Engine");
+        train.add("Sleeper");
+        train.add("AC");
+        train.add("Cargo");
+        train.add("Guard");
 
-        int singleAvailable = 5;
-        int doubleAvailable = 3;
-        int suiteAvailable = 2;
+        System.out.println("Initial Train Consist:");
+        System.out.println(train);
+        train.add(2, "Pantry");
 
-        System.out.println("=== Hotel Room Details ===\n");
-
-        single.displayRoomDetails();
-        System.out.println("Available: " + singleAvailable + "\n");
-
-        doubleRoom.displayRoomDetails();
-        System.out.println("Available: " + doubleAvailable + "\n");
-
-        suite.displayRoomDetails();
-        System.out.println("Available: " + suiteAvailable + "\n");
-
-        System.out.println("Application Finished.");
+        System.out.println("\nAfter adding Pantry at position 2:");
+        System.out.println(train);
+        train.removeFirst();
+        train.removeLast();
+        System.out.println("\nFinal Train Consist:");
+        System.out.println(train);
     }
 }
